b69a91dbce851446cbc466b059fb013b56687ec5
lilen <lilen@shsh.ylc.edu.tw>
2013-10-11 16:53:57 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Groupmy::Application.config.secret_token = 'c3069ed95f954823ca33b5b6ecc07bcdc7d3039bf7accd99391d41d31a15b3220f4c54db68ce3cdfdc4f1fe9599528d7834dcaacce42441a35527a9d49dca1df'
